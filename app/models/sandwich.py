80c4a85212b9e634a48409618ffb42245bd46722
markx92 <164291756+markx92@users.noreply.github.com>
2024-05-06 19:02:28 -0400

    commit sandwich

--- app/models/sandwich.py
+++ app/models/sandwich.py
@@ -5,13 +5,13 @@ class sandwich(BaseModel):
 
     SHEET_NAME = "sandwiches"
 
-    COLUMNS = ["sandwich", "price", "filling", "photo"]
+    COLUMNS = ["name", "price", "filling", "photo"]
 
     SEEDS = [
-        {"sandwich": "Philly Cheesesteak", "price": "12.99", "filling": "thin sliced steak with melted provolone cheese, white American cheese, or Cheez Whiz, as well as fried onions", "photo": "https://shorturl.at/aCMUY"},
-        {"sandwich": "Meatball Sub", "price": "13.99", "filling": "fresh made beef and pork meatballs, marinara sayce, melty provolone cheese", "photo": "https://shorturl.at/buU26"},
-        {"sandwich": "Caprese Sandwich", "price": "9.99", "filling": "mozzarella, tomato, basil, and the fixins", "photo": "https://shorturl.at/BKQW8"},
-        {"sandwich": "The Original Italian", "price": "11.99", "filling": "morty-d, salami, provolone, onions, pickles, vinegar, lettuce and tomato, can't get any better", "photo": "https://shorturl.at/empZ9"},
+        {"name": "Philly Cheesesteak", "price": "12.99", "filling": "thin sliced steak with melted provolone cheese, white American cheese, or Cheez Whiz, as well as fried onions", "photo": "https://shorturl.at/aCMUY"},
+        {"name": "Meatball Sub", "price": "13.99", "filling": "fresh made beef and pork meatballs, marinara sayce, melty provolone cheese", "photo": "https://shorturl.at/buU26"},
+        {"name": "Caprese Sandwich", "price": "9.99", "filling": "mozzarella, tomato, basil, and the fixins", "photo": "https://shorturl.at/BKQW8"},
+        {"name": "The Original Italian", "price": "11.99", "filling": "morty-d, salami, provolone, onions, pickles, vinegar, lettuce and tomato, can't get any better", "photo": "https://shorturl.at/empZ9"},
     ]
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     print("FOUND", len(sandwiches), "SANDWICHES")
     if any(sandwiches):
         for sandwich in sandwiches:
-            print(sandwich.sandwich, sandwich.price, sandwich.filling, sandwich.photo)
+            print(sandwich.name, sandwich.price, sandwich.filling, sandwich.photo)
     else:
-        Sandwich.seed()
+        sandwich.seed()
 
